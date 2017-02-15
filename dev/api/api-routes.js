42356c80ac4a49f254bc594d7fc5101ba031da88
Steve Chronister <stephen.g.chronister@gmail.com>
2017-02-15 17:09:06 -0600

    user model

--- dev/api/api-routes.js
+++ dev/api/api-routes.js
@@ -1,5 +1,6 @@
 var bodyParser = require("body-parser");
 var Article = require("../models/Article");
+var User = require("../models/User");
 var path = require("path");
 
 module.exports = function(app){
@@ -12,7 +13,29 @@ module.exports = function(app){
     // getting all articles
 
     app.get("/api/articles", function(req, res){
+    	console.log("Yer gettin all the articles!");
     })
+
+    //get all articles of a category
+    app.get("/api/articles/category/:category", function(req,res){
+    	console.log("Getting all articles of " + req.params.category);
+
+    })
+
+    app.get("/api/articles/id/:id", function(req, res){
+    	console.log("Getting article with id "+ req.params.id);
+    	Article.findById(req.params.id, function(err, doc){
+    		if (err) throw err;
+    		res.json(doc);
+    	})
+    })
+
+    //user authentication
+    
+
+
+
+
 }
 
 
---
+++ dev/models/User.js
@@ -0,0 +1,50 @@
+// Require mongoose
+var mongoose = require("mongoose");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+var bcrypt = require(bcrypt),
+var SALT_WORK_FACTOR = 10;
+
+// Create article schema
+var UserSchema = new Schema({
+  username: { type: String, required: true, index: { unique: true } },
+  password: { type: String, required: true },
+  name:{type: String, required:true},
+  points:{type: Number, default:0, required:true}
+});
+
+UserSchema.pre(save, function(next) {
+    var user = this;
+
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+      if (err) return next(err);
+
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) return next(err);
+
+          // override the cleartext password with the hashed one
+          user.password = hash;
+          next();
+      });
+    });
+
+
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+var User = mongoose.model("User", UserSchema);
+
+// Export the model
+module.exports = User;
